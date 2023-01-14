1773e09cc438984389cdbe1906bc624c856c50fa
Satish0915 <satushdhanani@gmail.com>
2023-01-14 08:39:47 +0000

    updated read me

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Azure-Resume
 My Own Azure Resume - Following a Azure Cloud Project
+
+
+## First Step
+- Frotend folder contains website.
+- main.js created got visitor counter code.
+
--- frontend/main.js
+++ frontend/main.js
@@ -1,14 +1,20 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    getVisitCount();
+})
+
 const functionApi = '';
 
-const getVisitCount = () =>{
+const getVisitCount = () => {
         let count = 30;
-        fetch(functionApi).then(resonse =>{
-            return Response.json()
+        fetch(functionApi).then(response => {
+            return response.json()
         }).then(response =>{
             console.log("website called fucntion API.");
             count = response.count;
             document.getElementById("counter").innerText = count;
-        }
-            )
+        }).catch(function(error){
+            console.log(error);
+        });
 
+        return count;
 }
